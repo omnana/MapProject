090ae0752e9f2cdf1c53d53f3e8d5a4a0c7d7285
李泽洋 <1476264355@qq.com>
2021-02-26 18:31:55 +0800

    下载资源

--- Assets/Code/AssetLoad/DownloadMgr/DownloadFileMac.cs
+++ Assets/Code/AssetLoad/DownloadMgr/DownloadFileMac.cs
@@ -1,18 +1,326 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using UnityEngine;
 
-public class DownloadFileMac : MonoBehaviour
+public enum DownloadMacState
 {
-    // Start is called before the first frame update
-    void Start()
+    None,
+    ResetSize,
+    Download,
+    Md5,
+    Complete,
+    Error,
+}
+
+public class DownloadFileMac
+{
+    private const int OneReadLen = 16 * 1024;       // 一次读取长度 16384 = 16*kb
+
+    private const int Md5ReadLen = 16 * 1024;       // 一次读取长度 16384 = 16*kb
+
+    private const int ReadWriteTimeOut = 2 * 1000;  // 超时等待时间
+
+    private const int TimeOutWait = 5 * 1000;       // 超时等待时间
+
+    public DownloadUnit DownUnit { get; private set; }
+
+    public DownloadMacState State = DownloadMacState.None;
+
+    public string Error { get; set; }
+
+    public int CurSize { get; private set; }
+
+    public int AllSize { get; private set; }
+
+    public int TryCount { get; private set; }
+
+    public DownloadFileMac(DownloadUnit unit)
     {
-        
+        DownUnit = unit;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Run()
     {
-        
+        TryCount++;
+
+        //State = DownloadMacState.ResetSize;
+
+        //if (!ResetSize()) return;
+
+        State = DownloadMacState.Download;
+
+        if (!DownLoad()) return;
+
+        State = DownloadMacState.Md5;
+
+        if (!CheckMd5()) // 校验失败，重下一次
+        {
+            State = DownloadMacState.Download;
+
+            if (!DownLoad()) return;
+
+            State = DownloadMacState.Md5;
+
+            if (!CheckMd5()) return; // 两次都失败，文件有问题
+        }
+
+        State = DownloadMacState.Complete;
+    }
+
+    /// <summary>
+    /// 重置文件长度
+    /// </summary>
+    /// <returns></returns>
+    private bool ResetSize()
+    {
+        if(DownUnit.Size <= 0)
+        {
+            DownUnit.Size = GetWebFileSize(DownUnit.DownUrl);
+
+            if (DownUnit.Size == 0) return false;
+        }
+
+        CurSize = 0;
+
+        AllSize = DownUnit.Size;
+
+        return true;
+    }
+
+
+    private bool CheckMd5()
+    {
+        if (string.IsNullOrEmpty(DownUnit.Md5)) return true; //不做校验，默认成功
+
+        string md5 = GetMD5HashFromFile(DownUnit.SavePath);
+
+        if (md5 != DownUnit.Md5)
+        {
+            File.Delete(DownUnit.SavePath);
+
+            //ThreadDebugLog.Log("文件MD5校验出错：" + Unit.name);
+
+            State = DownloadMacState.Error;
+
+            Error = "Check MD5 Error ";
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private bool DownLoad()
+    {
+        //打开上次下载的文件
+        long startPos = 0;
+
+        var tempFile = DownUnit.SavePath + ".temp";
+
+        FileStream fs = null;
+
+        if (File.Exists(DownUnit.SavePath)) // 文件已存在，跳过
+        {
+            CurSize = DownUnit.Size;
+
+            return true;
+        }
+        else if (File.Exists(tempFile)) // 以及有部分下载到本地
+        {
+            fs = File.OpenWrite(tempFile);
+
+            startPos = fs.Length;
+
+            fs.Seek(startPos, SeekOrigin.Current); // 移动文件流中的当前指针
+
+            if (startPos == DownUnit.Size) // 文件已经下载完，没改名字，结束
+            {
+                fs.Flush();
+
+                fs.Close();
+
+                fs = null;
+
+                if (File.Exists(DownUnit.SavePath)) File.Delete(DownUnit.SavePath);
+
+                File.Move(tempFile, DownUnit.SavePath);
+
+                CurSize = (int)startPos;
+
+                return true;
+            }
+        }
+        else
+        {
+            var direName = Path.GetDirectoryName(tempFile);
+
+            if (!Directory.Exists(direName)) Directory.CreateDirectory(direName);
+
+            fs = new FileStream(tempFile, FileMode.Create);
+        }
+
+        // 下载逻辑
+
+        HttpWebRequest request = null;
+
+        HttpWebResponse respone = null;
+
+        Stream ns = null;
+
+        try
+        {
+            request = WebRequest.Create(DownUnit.DownUrl) as HttpWebRequest;
+
+            request.ReadWriteTimeout = ReadWriteTimeOut;
+
+            request.Timeout = TimeOutWait;
+
+            // 向服务器请求，获得服务器回应数据流
+            if (startPos > 0) request.AddRange((int)startPos); // 设置Range值，断点续传
+
+            respone = (HttpWebResponse)request.GetResponse();
+
+            ns = respone.GetResponseStream();
+
+            ns.ReadTimeout = TimeOutWait;
+
+            var totalSize = respone.ContentLength;
+
+            var curSize = startPos;
+
+            if(curSize == totalSize)
+            {
+                fs.Flush();
+
+                fs.Close();
+
+                if (File.Exists(DownUnit.SavePath)) File.Delete(DownUnit.SavePath);
+
+                File.Move(tempFile, DownUnit.SavePath);
+
+                CurSize = (int)startPos;
+            }
+            else
+            {
+                var bytes = new byte[OneReadLen];
+
+                var readSize = ns.Read(bytes, 0, OneReadLen); // 读取第一份数据
+
+                while(readSize > 0)
+                {
+                    fs.Write(bytes, 0, readSize); // 将下载到的数据写入临时文件
+
+                    curSize += readSize;
+
+                    // 判断是否下载完成
+                    // 下载完成将temp文件，改成正式文件
+                    if (curSize == totalSize)
+                    {
+                        fs.Flush();
+
+                        fs.Close();
+
+                        fs = null;
+
+                        if (File.Exists(DownUnit.SavePath)) File.Delete(DownUnit.SavePath);
+
+                        File.Move(tempFile, DownUnit.SavePath);
+                    }
+
+                    // 回调一下
+                    CurSize = (int)curSize;
+
+                    // 往下继续读取
+                    readSize = ns.Read(bytes, 0, OneReadLen);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            //下载失败，删除临时文件
+            if (fs != null) { fs.Flush(); fs.Close(); fs = null; }
+
+            if (File.Exists(tempFile))
+                File.Delete(tempFile);
+
+            if (File.Exists(DownUnit.SavePath))
+                File.Delete(DownUnit.SavePath);
+
+            State = DownloadMacState.Error;
+
+            Error = string.Format("Download Error {0}", ex.Message);
+        }
+        finally
+        {
+            if (fs != null) { fs.Flush(); fs.Close(); fs = null; }
+
+            if (ns != null) { ns.Close(); ns = null; }
+
+            if (respone != null) { respone.Close(); respone = null; }
+
+            if (request != null) { request.Abort(); request = null; }
+        }
+
+        if (State == DownloadMacState.Error) return false;
+
+        return true;
+    }
+
+    private string GetMD5HashFromFile(string fileName)
+    {
+        byte[] buffer = new byte[Md5ReadLen];
+
+        int readLength = 0;//每次读取长度
+
+        var output = new byte[Md5ReadLen];
+
+        using (Stream inputStream = File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+        {
+            using (System.Security.Cryptography.HashAlgorithm hashAlgorithm = new System.Security.Cryptography.MD5CryptoServiceProvider())
+            {
+                while ((readLength = inputStream.Read(buffer, 0, buffer.Length)) > 0) // 计算MD5
+                {
+                    hashAlgorithm.TransformBlock(buffer, 0, readLength, output, 0);
+                }
+
+                //完成最后计算，必须调用(由于上一部循环已经完成所有运算，所以调用此方法时后面的两个参数都为0)  
+                hashAlgorithm.TransformFinalBlock(buffer, 0, 0);
+
+                var retVal = hashAlgorithm.Hash;
+
+                var sb = new System.Text.StringBuilder(32);
+
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+
+                hashAlgorithm.Clear();
+
+                inputStream.Close();
+
+                return sb.ToString();
+            }
+        }
+    }
+
+    private int GetWebFileSize(string url)
+    {
+        return 0;
+    }
+
+    /// <summary>
+    /// 防止失败频繁回调，只在特定次数回调
+    /// </summary>
+    /// <returns></returns>
+    public bool IsNeedErrorCall()
+    {
+        if (TryCount == 3 || TryCount == 10 || TryCount == 100)
+            return true;
+
+        return false;
     }
 }
--- Assets/Code/AssetLoad/DownloadMgr/DownloadMgr.cs
+++ Assets/Code/AssetLoad/DownloadMgr/DownloadMgr.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using System.Net;
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+
+/*
+ 在游戏开发中，热更并下载资源，对商业化游戏来说是一个必须的需求。
+ 而想要高效并稳定地下载文件一直是开发中的一个痛点我们能看到市面
+ 上，大量游戏都在下载上卡死，断网，重启无效等等问题。我们来总结
+ 一下要解决的问题：
+（1）网络请求异常处理——断网、请求失败、请求超时、网络波动、下载到一半等问题
+（2）文件读写异常处理——文件读失败、文件写失败、文件写一半等问题
+（3）游戏进程异常处理——下载到一半、文件写一半、玩家退出等问题
+（4）重启现场恢复处理——可恢复性，继续下载
+（5）文件的下载正确性——文件长度、文件校验，文件可读性
+（6）文件线程高效下载——多线程，异步回调文件
+ */
 
 public class DownloadMgr
 {
@@ -27,6 +43,36 @@ public class DownloadMgr
         completeList = new List<DownloadUnit>();
 
         errorList = new List<DownloadFileMac>();
+
+        //https解析的设置
+        ServicePointManager.DefaultConnectionLimit = 100;
+
+        ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCallback;
+    }
+
+    private bool RemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+    {
+        bool isOk = true;
+        // If there are errors in the certificate chain, look at each error to determine the cause.
+        if (sslPolicyErrors != SslPolicyErrors.None)
+        {
+            for (int i = 0; i < chain.ChainStatus.Length; i++)
+            {
+                if (chain.ChainStatus[i].Status != X509ChainStatusFlags.RevocationStatusUnknown)
+                {
+                    chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                    chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                    chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 1, 0);
+                    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                    bool chainIsValid = chain.Build((X509Certificate2)certificate);
+                    if (!chainIsValid)
+                    {
+                        isOk = false;
+                    }
+                }
+            }
+        }
+        return isOk;
     }
 
     /// <summary>
@@ -35,6 +81,26 @@ public class DownloadMgr
     /// <param name="info"></param>
     public void DownloadAsync(DownloadUnit info)
     {
+        if (info == null) return;
+
+        var fileMac = new DownloadFileMac(info);
+
+        lock (mlock)
+        {
+            readyList.Enqueue(fileMac);
+        }
+
+        if (runningList.Count < MAX_THREAD_COUNT)
+        {
+            var thread = new Thread(ThreadLoop);
+
+            lock (mlock)
+            {
+                runningList.Add(thread, null);
+            }
+
+            thread.Start();
+        }
     }
 
     /// <summary>
@@ -44,10 +110,299 @@ public class DownloadMgr
     /// <returns></returns>
     public bool DownloadSync(DownloadUnit info)
     {
+        if (info == null) return false;
+
+        var mac = new DownloadFileMac(info);
+
+        try // 同步下载尝试三次
+        {
+            mac.Run();
+            if (mac.State == DownloadMacState.Complete) return true;
+            mac.Run();
+            if (mac.State == DownloadMacState.Complete) return true;
+            mac.Run();
+            if (mac.State == DownloadMacState.Complete) return true;
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("Error DownloadSync " + mac.State + " " + mac.DownUnit.Name + " " + ex.Message + " " + ex.StackTrace);
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// 删除下载
+    /// </summary>
+    /// <param name="info"></param>
+    public void DeleteDownload(DownloadUnit info)
+    {
+        lock (mlock)
+        {
+            info.IsDelete = true;
+        }
+    }
+
+    //清理所有下载
+    public void ClearAllDownloads()
+    {
+        lock (mlock)
+        {
+            foreach (var mac in readyList)
+            {
+                if (mac != null) mac.DownUnit.IsDelete = true;
+            }
+
+            foreach (var item in runningList)
+            {
+                if (item.Value != null) item.Value.DownUnit.IsDelete = true;
+            }
+
+            foreach (var unit in completeList)
+            {
+                if (unit != null) unit.IsDelete = true;
+            }
+        }
+    }
+
+    private void ThreadLoop()
+    {
+        while (true)
+        {
+            DownloadFileMac mac = null;
+
+            lock (mlock)
+            {
+                if (readyList.Count > 0)
+                {
+                    mac = readyList.Dequeue();
+
+                    runningList[Thread.CurrentThread] = mac;
+
+                    if (mac != null && mac.DownUnit.IsDelete) // 已经销毁，不提取运行，直接删除
+                    {
+                        runningList[Thread.CurrentThread] = null;
+
+                        continue;
+                    }
+                }
+            }
+
+            // 已经没有需要下载的了
+            if (mac == null) break;
+
+            mac.Run();
+
+            if (mac.State == DownloadMacState.Complete)
+            {
+                lock (mlock)
+                {
+                    completeList.Add(mac.DownUnit);
+
+                    runningList[Thread.CurrentThread] = null;
+                }
+
+            }
+            else if (mac.State == DownloadMacState.Error)
+            {
+                lock (mlock)
+                {
+                    readyList.Enqueue(mac);
+                    // 防止失败频繁回调，只在特定次数回调
+                    if (mac.IsNeedErrorCall())
+                    {
+                        errorList.Add(mac);
+                    }
+                }
+
+                break;
+            }
+            else
+            {
+                //ThreadDebugLog.Log("Error DownloadMacState " + mac.State + " " + mac.DownUnit.name);
+                break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 回调完成
+    /// </summary>
+    private void UpdateComplete()
+    {
+        if (completeList.Count == 0) return;
+
+        DownloadUnit[] completes = null;
+
+        lock (mlock)
+        {
+            completes = completeList.ToArray();
+
+            completeList.Clear();
+        }
+
+        for(var i = 0; i< completes.Length; i++)
+        {
+            var info = completeList[i];
+
+            if (info.IsDelete) continue;
+
+            info.IsDelete = true;
+
+            info.ProgressFun?.Invoke(info, info.Size, info.Size);
+
+            if (info.CompleteFun != null)
+            {
+                try
+                {
+                    info.CompleteFun(info);
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError("UpdateComplete " + ex.Message);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void UpdateError()
+    {
+        if (errorList.Count == 0) return;
+
+        DownloadFileMac[] errorArr = null;
+
+        lock (mlock)
+        {
+            errorArr = errorList.ToArray();
+
+            errorList.Clear();
+        }
+
+        foreach (var mac in errorArr)
+        {
+            var info = mac.DownUnit;
+
+            if (info.IsDelete) continue; //已经销毁，不进行回调
+
+            if (info.ErrorFun != null)
+            {
+                info.ErrorFun(info, mac.Error);
+
+                mac.Error = string.Empty;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void UpdateProgress()
+    {
+        if (runningList.Count == 0) return;
+
+        var runArr = new List<DownloadFileMac>();
+
+        lock (mlock)
+        {
+            foreach (var mac in runningList.Values)
+            {
+                if (mac != null) runArr.Add(mac);
+            }
+        }
+
+        foreach (var mac in runArr)
+        {
+            var info = mac.DownUnit;
+
+            if (info.IsDelete) continue; //已经销毁，不进行回调
+
+            info.ProgressFun?.Invoke(info, mac.CurSize, mac.AllSize);
+        }
+    }
+
+    /// <summary>
+    /// 线程遍历
+    /// 线程遍历就三个逻辑：关闭卡死线程、网络断开处理和开启新线程。
+    /// 实现了线程的动态创建和销毁。
+    /// </summary>
+    private void UpdateThread()
+    {
+        if (readyList.Count == 0 && runningList.Count == 0) return;
+
+        //关闭卡死的线程
+        lock (mlock)
+        {
+            var threadList = new List<Thread>();
+
+            foreach (var item in runningList)
+            {
+                if (item.Key.IsAlive) continue; // 已经在运行的跳过
+
+                if (item.Value != null) readyList.Enqueue(item.Value);
+
+                threadList.Add(item.Key);
+            }
+
+            foreach (var thread in threadList)
+            {
+                runningList.Remove(thread);
+
+                thread.Abort();
+            }
+        }
+
+        //网络断开处理
+        if (!CheckNetworkActive()) return;
+
+        //线程数量不足，创建
+        if (runningList.Count >= MAX_THREAD_COUNT) return;
+
+
+        if(readyList.Count > 0)
+        {
+            var thread = new Thread(ThreadLoop);
+
+            lock (mlock)
+            {
+                runningList.Add(thread, null);
+            }
+
+            thread.Start();
+        }
+    }
+
+    /// <summary>
+    /// 是否有网络
+    /// </summary>
+    /// <returns></returns>
+    private bool CheckNetworkActive()
+    {
+        if (Application.internetReachability == NetworkReachability.NotReachable) // 没有网络
+        {
+            return false;
+        }
+
+        if (Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork) // 234G网络
+        {
+            return true;
+        }
+
+        if (Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork) // wifi网络
+        {
+            return true;
+        }
+
         return false;
     }
 
     public void Update()
     {
+        UpdateComplete();
+        UpdateProgress();
+        UpdateError();
+        UpdateThread();
     }
 }
